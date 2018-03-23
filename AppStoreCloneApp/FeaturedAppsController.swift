05fed0cc5221546625be53ea81d4042145025102
Udit Kapahi <udit.kapahi@squareyards.co.in>
2018-03-23 12:49:18 +0530

    Setting up the layout.Not using the storyboards,instantiating the layout programatically

---
+++ AppStoreCloneApp/FeaturedAppsController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  AppStoreCloneApp
+//
+//  Created by Udit Kapahi on 23/03/18.
+//  Copyright © 2018 Udit Kapahi. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppsController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:  "cellId", for: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    //overriden from UICollectionViewDelegateFlowLayout, helps to specify the size of cell
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
+}
+
+
+
+class CategoryCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init coder has not been implemented")
+    }
+    
+    func setupViews()  {
+        backgroundColor = UIColor.black
+    }
+}
+
